773c28c9bacb4c94ee48b7dc603a767c4f2afbe9
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-31 20:27:58 -0300

    refactor: redefine Action type and update countReducer to handle increment and decrement actions in Counter component

--- exercises/01.use-reducer/05.problem.traditional/index.tsx
+++ exercises/01.use-reducer/05.problem.traditional/index.tsx
@@ -2,29 +2,34 @@ import { useReducer, useState } from 'react'
 import * as ReactDOM from 'react-dom/client'
 
 type State = { count: number }
-// 🐨 make it so the action is one of two objects:
-// - a type string with the value 'increment' and a step number with the value of the step
-// - a type string with the value 'decrement' and a step number with the value of the step
-type Action = Partial<State> | ((currentState: State) => Partial<State>)
-// 🐨 update the countReducer to handle the new action type
-// 💯 handle situations where the action's type is neither increment nor decrement
-const countReducer = (state: State, action: Action) => ({
-	...state,
-	...(typeof action === 'function' ? action(state) : action),
-})
+type Action =
+	| { type: 'increment'; step: number }
+	| { type: 'decrement'; step: number }
+function countReducer(state: State, action: Action) {
+	const { type, step } = action
+	switch (type) {
+		case 'increment': {
+			return {
+				...state,
+				count: state.count + step,
+			}
+		}
+		case 'decrement': {
+			return {
+				...state,
+				count: state.count - step,
+			}
+		}
+	}
+}
 
 function Counter({ initialCount = 0, step = 1 }) {
-	// 🐨 rename "setState" to "dispatch"
-	const [state, setState] = useReducer(countReducer, {
+	const [state, dispatch] = useReducer(countReducer, {
 		count: initialCount,
 	})
 	const { count } = state
-	// 🐨 the logic has now been moved back to the reducer, update these to pass
-	// the appropriate action object to the dispatch function
-	const increment = () =>
-		setState((currentState) => ({ count: currentState.count + step }))
-	const decrement = () =>
-		setState((currentState) => ({ count: currentState.count - step }))
+	const increment = () => dispatch({ type: 'increment', step })
+	const decrement = () => dispatch({ type: 'decrement', step })
 	return (
 		<div className="counter">
 			<output>{count}</output>
