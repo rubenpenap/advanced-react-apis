76e7d3535e7b757e5942b064000759a7b0f25470
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-03-31 19:13:36 -0300

    refactor: rename variables in countReducer and update dispatch functions in Counter component

--- exercises/01.use-reducer/02.problem.previous-state/index.tsx
+++ exercises/01.use-reducer/02.problem.previous-state/index.tsx
@@ -1,17 +1,12 @@
 import { useReducer, useState } from 'react'
 import * as ReactDOM from 'react-dom/client'
 
-// 🐨 rename the "state" variable "count" and the "newState" should be "change"
-// 🐨 then the function should return the sum of "count" and "change"
-const countReducer = (state: unknown, newState: number) => newState
+const countReducer = (count: number, change: number) => count + change
 
 function Counter({ initialCount = 0, step = 1 }) {
-	// 🐨 change the dispatch function "setCount" to "changeCount" here
-	const [count, setCount] = useReducer(countReducer, initialCount)
-	// 🐨 update these to simply pass the change we want to make to the state rather
-	// than the new state itself.
-	const increment = () => setCount(count + step)
-	const decrement = () => setCount(count - step)
+	const [count, changeCount] = useReducer(countReducer, initialCount)
+	const increment = () => changeCount(step)
+	const decrement = () => changeCount(-step)
 	return (
 		<div className="counter">
 			<output>{count}</output>
