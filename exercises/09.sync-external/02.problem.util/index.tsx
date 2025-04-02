6ecada7c90e01f477472c8c64de3e607f02b3397
RUBEN PEÑA PARADA <46791460+rubenpenap@users.noreply.github.com>
2025-04-02 17:12:02 -0300

    refactor: encapsulate media query logic in makeMediaQueryStore function for improved reusability in NarrowScreenNotifier component

--- exercises/09.sync-external/02.problem.util/index.tsx
+++ exercises/09.sync-external/02.problem.util/index.tsx
@@ -3,28 +3,28 @@ import * as ReactDOM from 'react-dom/client'
 
 const mediaQuery = '(max-width: 600px)'
 
-// 🐨 put getSnapshot and subscribe in a new function called makeMediaQueryStore
-// which accepts a mediaQuery and returns a hook that uses useSyncExternalStore
-// with the subscribe and getSnapshot functions.
-function getSnapshot() {
-	return window.matchMedia(mediaQuery).matches
-}
+export function makeMediaQueryStore(mediaQuery: string) {
+	function getSnapshot() {
+		return window.matchMedia(mediaQuery).matches
+	}
 
-function subscribe(callback: () => void) {
-	const mediaQueryList = window.matchMedia(mediaQuery)
-	mediaQueryList.addEventListener('change', callback)
-	return () => {
-		mediaQueryList.removeEventListener('change', callback)
+	function subscribe(callback: () => void) {
+		const mediaQueryList = window.matchMedia(mediaQuery)
+		mediaQueryList.addEventListener('change', callback)
+		return () => {
+			mediaQueryList.removeEventListener('change', callback)
+		}
+	}
+
+	return function useMediaQuery() {
+		return useSyncExternalStore(subscribe, getSnapshot)
 	}
 }
-// 🐨 put everything above in the makeMediaQueryStore function
 
-// 🐨 call makeMediaQueryStore with '(max-width: 600px)' and assign the return
-// value to a variable called useNarrowMediaQuery
+const useNarrowMediaQuery = makeMediaQueryStore(mediaQuery)
 
 function NarrowScreenNotifier() {
-	// 🐨 call useNarrowMediaQuery here instead of useSyncExternalStore
-	const isNarrow = useSyncExternalStore(subscribe, getSnapshot)
+	const isNarrow = useNarrowMediaQuery()
 	return isNarrow ? 'You are on a narrow screen' : 'You are on a wide screen'
 }
 
